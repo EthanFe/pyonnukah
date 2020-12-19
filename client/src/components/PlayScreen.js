3f59b9212e2bccb8510f719da6ca7b64d7d7a8fd
EthanFe <sneakcow@gmail.com>
2020-12-18 23:57:15 -0600

    candle rendering is a GO

--- client/src/components/PlayScreen.js
+++ client/src/components/PlayScreen.js
@@ -59,7 +59,7 @@ const Menorah = ({position, candles, litCandles}) => {
   return (
     <>
       <Shamus position={position} dimensions={dimensions}/>
-      <Candles position={position} dimensions={dimensions} candles={8}/>
+      <Candles position={position} dimensions={dimensions} candles={7} litCandles={{0: true, 1: true, 2: false, 3: true, 4: false, 5: true, 6: true, 7: true}}/>
       <img
         style={styles.image(position, dimensions)}
         src={menorahImage}
@@ -98,22 +98,45 @@ const Candles = ({position, dimensions, candles, litCandles}) => {
     {left: 11.5, right: 62},
     {left: 0, right: 62}
   ]
+
+  const flamePositionOffsets = [
+    {x: ((900 - 208) / 900) * 1000, y: ((600 - 305) / 600) * 1000},
+    {x: ((900 - 251) / 900) * 1000, y: ((600 - 305) / 600) * 1000},
+    {x: ((900 - 298) / 900) * 1000, y: ((600 - 305) / 600) * 1000},
+    {x: ((900 - 344) / 900) * 1000, y: ((600 - 305) / 600) * 1000},
+    {x: ((900 - 555) / 900) * 1000, y: ((600 - 305) / 600) * 1000},
+    {x: ((900 - 600) / 900) * 1000, y: ((600 - 305) / 600) * 1000},
+    {x: ((900 - 648) / 900) * 1000, y: ((600 - 305) / 600) * 1000},
+    {x: ((900 - 692) / 900) * 1000, y: ((600 - 305) / 600) * 1000},
+  ]
+  console.log(flamePositionOffsets)
+
+  const candleFlames = Object.keys(litCandles).filter(index => litCandles[index]).map(index =>
+    <img
+      style={styles.image({x: flamePositionOffsets[index].x, y: flamePositionOffsets[index].y}, {width: 70, height: 91})}
+      src={candleFlameImage}
+      alt=""
+    />
+  )
   const cutoff = cutoffValues[candles]
   const clipPathValue = `polygon(${cutoff.left}% 100%, ${cutoff.left}% 0%, 38% 0%, 38% 100%, ${cutoff.right}% 100%, ${cutoff.right}% 0%, 100% 0%, 100% 100%)`
   console.log(clipPathValue)
   return (
-    <>
+    <span style={{position: "relative"}}>
       <img
         style={{...styles.image(position, dimensions), clipPath: clipPathValue}}
         src={candlesImage}
         alt=""
       />
-      <img
+      <>
+        {candleFlames}
+      </>
+      {/* <img
         style={{...styles.image(position, dimensions), clipPath: clipPathValue}}
         src={candleFlamesImage}
         alt=""
-      />
-    </>
+      /> */}
+    </span>
   )
 }
 
